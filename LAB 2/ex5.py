228aee4f17ec6150f3e93df61d962133aaadf88a
Khaled Jalal Al-arashi <77218749+kh-cs@users.noreply.github.com>
2024-05-06 22:19:48 +0300

    Add files via upload

---
+++ LAB 2/ex5.py
@@ -0,0 +1,5 @@
+sub1 = int(input("Enter the mark of subject 1 : "))
+sub2 = int(input("Enter the mark of subject 2 : "))
+sub3 = int(input("Enter the mark of subject 3 : "))
+avg = (sub1+sub2+sub3)/3
+print("The Average is : ",avg,"%")
