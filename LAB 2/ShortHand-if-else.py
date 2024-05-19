9347c9514dae978107f89366264d0115493f928f
Khaled Jalal Al-arashi <77218749+kh-cs@users.noreply.github.com>
2024-05-19 22:44:19 +0300

    Delete LAB 2/ShortHand-if-else.py

--- LAB 2/ShortHand-if-else.py
+++
@@ -1,5 +0,0 @@
-x = int(input("Enter the number : "))
-y = int(input("Enter the number : "))
-z = int(input("Enter the number : "))
-
-print(x) if x>=y and x>=z else ( print(y) if y>=x and y>=z else print(z) )
